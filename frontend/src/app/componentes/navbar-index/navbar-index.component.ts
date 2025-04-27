7d7097554176351c032bc2d727d9eb239faa1f74
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-04-27 11:42:45 +0200

    crear componente para el navbar inicial

---
+++ frontend/src/app/componentes/navbar-index/navbar-index.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-index',
+  imports: [],
+  templateUrl: './navbar-index.component.html',
+  styleUrl: './navbar-index.component.css'
+})
+export class NavbarIndexComponent {
+
+}
