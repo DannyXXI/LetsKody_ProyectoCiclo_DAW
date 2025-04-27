f6493abeadb9db1069fa12e19ab04f7c2e210829
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-04-27 12:04:57 +0200

    configurar redirección a la página de inicio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
 
-export const routes: Routes = [];
+// archivo que se encarga de las rutas entre los componentes del proyecto de Angular
+export const routes: Routes = [
+    {path: "" , redirectTo: "home" , pathMatch: "full"}, // cuando carga redirecciona a /home
+    {path: "home" , component: HomeComponent} // ruta para la dirección para la página principal
+];
