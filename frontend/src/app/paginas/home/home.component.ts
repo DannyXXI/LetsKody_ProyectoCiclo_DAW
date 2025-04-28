bde09f739e7d58e08a901db708df018d4ef15c75
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-04-28 19:48:25 +0200

    agregar el html y css del navbar inicial

--- frontend/src/app/paginas/home/home.component.ts
+++ frontend/src/app/paginas/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarIndexComponent } from '../../componentes/navbar-index/navbar-index.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NavbarIndexComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
