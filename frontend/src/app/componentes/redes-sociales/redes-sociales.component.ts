80d5eb60632d82f6a8639a21130c1d2e0fadea0a
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-05-11 13:50:36 +0200

    agregar componente de redes sociales y estructura base del html del home

---
+++ frontend/src/app/componentes/redes-sociales/redes-sociales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redes-sociales',
+  imports: [],
+  templateUrl: './redes-sociales.component.html',
+  styleUrl: './redes-sociales.component.css'
+})
+export class RedesSocialesComponent {
+
+}
--- frontend/src/app/paginas/home/home.component.ts
+++ frontend/src/app/paginas/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarIndexComponent } from '../../componentes/navbar-index/navbar-index.component';
+import { RedesSocialesComponent } from '../../componentes/redes-sociales/redes-sociales.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarIndexComponent],
+  imports: [NavbarIndexComponent, RedesSocialesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
