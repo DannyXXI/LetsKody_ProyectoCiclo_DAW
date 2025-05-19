f3ff445873faafc7de41caf76ba8c7f0eb5baf5a
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-05-19 23:10:41 +0200

    se agrega el html y css del componente faq

--- frontend/src/app/paginas/faq/faq.component.ts
+++ frontend/src/app/paginas/faq/faq.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarIndexComponent } from '../../componentes/navbar-index/navbar-index.component';
+import { RedesSocialesComponent } from '../../componentes/redes-sociales/redes-sociales.component';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
+  imports: [NavbarIndexComponent, RedesSocialesComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
