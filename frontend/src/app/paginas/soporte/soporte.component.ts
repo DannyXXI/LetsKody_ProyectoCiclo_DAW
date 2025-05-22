2255b5c0eb2546d4f6936a63f3ac8222894f0435
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-05-22 23:39:22 +0200

    se agrega el html y css del componente de soporte

--- frontend/src/app/paginas/soporte/soporte.component.ts
+++ frontend/src/app/paginas/soporte/soporte.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarIndexComponent } from '../../componentes/navbar-index/navbar-index.component';
+import { RedesSocialesComponent } from '../../componentes/redes-sociales/redes-sociales.component';
 
 @Component({
   selector: 'app-soporte',
-  imports: [],
+  imports: [NavbarIndexComponent, RedesSocialesComponent],
   templateUrl: './soporte.component.html',
   styleUrl: './soporte.component.css'
 })
