21e3c25080cd930417869485da89565ebed8caf9
Daniel Requejo Expósito <daniel.reqexp@gmail.com>
2025-05-25 18:16:47 +0200

    se agrega html, css y algo de ts del componente login

--- frontend/src/app/paginas/login/login.component.ts
+++ frontend/src/app/paginas/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { NavbarIndexComponent } from '../../componentes/navbar-index/navbar-index.component';
+import { RedesSocialesComponent } from '../../componentes/redes-sociales/redes-sociales.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NavbarIndexComponent, RedesSocialesComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  public mensajeError: string;  // variable para el mensaje de error
+
+  // método constructor del componente (inicializa variables)
+  constructor(){
+    this.mensajeError = "";
+  }
+
+  // función para abrir el modal en caso de error en el login (todavia no desarrollado la funcion completa)
+  public abrirModal() {
+    const dialog = document.getElementById("modalError") as HTMLDialogElement;
+    this.mensajeError = "mensaje de error si no puedes acceder"
+    if (dialog) {
+      dialog.showModal();
+    }
+  }
 
 }
